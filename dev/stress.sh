dc396eaf63c246e8722656d2da6f0456b00e9cba
sm222 <soleil.maraska@gmail.com>
2024-03-08 00:46:52 -0500

    add new scrip

--- dev/stress.sh
+++ dev/stress.sh
@@ -8,16 +8,16 @@ len=$#
 argv=("$@")
 name=""
 sys=$(uname -s)
+pwdtest=$(pwd)
+txt=$pwdtest
 
 # setting --<
 speed=0.2
 port=8080
 
-# debug --<
 
-#function --<
 function linux_ft_run() {
-  gnome-terminal -- $pwdtest/run.sh $txt
+  gnome-terminal -- $pwdtest/run.sh $pwdtest spam.sh
 }
 
 function mac_ft_run() {
@@ -32,42 +32,20 @@ function linux_start() {
 
 function mac_start() {
   osascript -e "tell app \"Terminal\"
-    do script \"$pwdtest/run.sh $txt\"
+    do script \"$pwdtest/run.sh spam.sh\"
   end tell"
 }
 
-function join_by {
-  local d=${1-} f=${2-}
-  if shift 2; then
-    printf %s "$f" "${@/#/$d}"
-  fi
-}
-
-# code --<
-#  start
-if [ $len == 0 ]
-  then
-  echo 'how to use -- ./dev.sh *.sh'
-  exit 1
-fi
-#  debug
-echo $# $sys
-#  set value
-pwdtest=$(pwd)
-txt=$pwdtest
-# remaking argv
-while [ $i -lt $# ]
-  do
-  txt=$(join_by  ' ' $txt ' ' ${argv[${i}]})
-  i=$((i + 1))
-done
-#   - - - - -   #
-sleep 1
 if [ $sys == "Darwin" ]
   then
     mac_start
     mac_ft_run
+    mac_ft_run
+    mac_ft_run
   else
     linux_start
+    sleep 1
+    linux_ft_run
+    linux_ft_run
     linux_ft_run
 fi
